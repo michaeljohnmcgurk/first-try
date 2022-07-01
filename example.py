0c0022f061e8c09b0741201425971de6a313e38c
Michael McGurk <michael.mcgurk@strath.ac.uk>
2022-07-01 11:30:03 +0100

    Initial commit

---
+++ example.py
@@ -0,0 +1,16 @@
+from numpy import *
+import openturns as ot
+from matplotlib import pyplot as plt
+from scipy.integrate import odeint
+import scipy as sp
+import openturns.viewer
+import openturns.viewer as viewer
+sps=sp.special
+
+# an example code to add to github
+
+y=4
+
+x=y**2+1
+
+q=x+y
